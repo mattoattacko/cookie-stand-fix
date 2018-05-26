a30128b69afea58d37b6d6146b2b10ea269b34b1
Matthew McQuain <matt.mcquain@gmail.com>
2018-05-26 15:27:20 -0700

    finally made my hours show up

--- app.js
+++ app.js
@@ -2,43 +2,38 @@
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
-//we need to create a place for our list of all locations to be placed when we create them
 var allLocations = [];
-//we need to define a variable that will be dynamic depending on how many cookies
 var totalCookiesByHour = 0;
-//totalTotal or netTotal
 var netTotal = 0;
+
+// allLocations[0].calcRandCustByHour();
 
 function MakeLocation(name, minCustPerHour, maxCustPerHour, avgCookiesSoldPerHour) {
     this.name = name;
     this.minCustPerHour = minCustPerHour;
     this.maxCustPerHour = maxCustPerHour;
     this.avgCookiesSoldPerHour = avgCookiesSoldPerHour;
+
     //these are the questions we need to solve
     this.randCustByHour = [];
     this.avgCookiesSoldPerHour = [];
     this.totalCookies = 0;
     allLocations.push(this);
+    this.calcRandCustByHour = function() {
+        for(var i = 0; i < hours.length; i++) {
+            this.randCustByHour.push(Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour);
+            console.log(this.randCustByHour[i]);
+        }
+    }
 
-    //this.calcRandCustByHour = function() {
-        //method go here
-        //use prototypes
-        //}
-
+    this.calcCookiesSoldByHour = function() {
+         for(var j = 0; j < hours.length; j++) {
+            this.cookiesSoldByHour.push(Math.round(this.avgCookiesSoldPerHour * this.randCustByHour[j]));
+            console.log(this.cookiesSoldByHour[j]);
+        }
+    }
 }
-CookieStore.prototype.populateCustomers = function () {
-    for (var i = 0; i < 14; i++) {
-      this.customersPerHour.push(randomCustomers(this.minCustomers, this.maxCustomers)); //populatecustomers is pushing the array of 14 numbers up into customers per hour []
-    }
-   };
-   
-CookieStore.prototype.calculateCookiesPerHour = function () {
-    this.populateCustomers();
-    for (var i = 0; i < 14; i++) {
-      this.cookiesPerHour.push(Math.ceil(this.customersPerHour[i] * this.averageCookies));
-      this.totalCookies = this.totalCookies + this.cookiesPerHour[i];
-    }
-   };
+        
 //remember to call the methods in the constructor that the now prototypes available to the new objects that the constructor will make using MakeLocation
 //this.calcRandCustByHour();
 
@@ -52,17 +47,17 @@ function makeStands() {
 
 makeStands();
 
-function makeHeaderRow() {
-    var cookiestands = document.getElementById('cookiedata');
-    var trEl = document.createElement('tr');
-    var trEl = document.createElement('th');
-    trEl.appendChild(thEl);
+// function makeHeaderRow() {
+//     var cookiestands = document.getElementById('cookiedata');
+//     var trEl = document.createElement('tr');
+//     var trEl = document.createElement('th');
+//     trEl.appendChild(thEl);
 
-}
+// }
 
 //table data
-var stuffForFirstRow = ["coffee", 2, 4, 6, 8, 10];
-var stuffForSecondRow = ["table", 3, 5, 7, 9, 1];
+var firstAndPikeTable = ["First and Pike", 2, 4, 6, 8, 10];
+var seatacAirportTable = ["Seatac Airport", 3, 5, 7, 9, 1];
 
 //access the table element in the html
 var tableEl = document.getElementById("cookiedata");
@@ -78,8 +73,10 @@ function tableRowMaker(inputArray) {
     }
     tableEl.appendChild(trEl); //add row to the table
 }
-tableRowMaker(stuffForFirstRow);
-tableRowMaker(stuffForSecondRow);
+
+tableRowMaker(hours);
+// tableRowMaker(firstAndPikeTable);
+// tableRowMaker(seatacAirportTable);
 //consider a for loop for the hours
 //create a th element
 //give the th element some text content like hours [i];
@@ -93,3 +90,16 @@ tableRowMaker(stuffForSecondRow);
 
 //remember to put an "empty" <th></th> element at the top of the table to push the "6am" cell over to the right.
 
+// CookieStore.prototype.populateCustomers = function () {
+//     for (var i = 0; i < 14; i++) {
+//       this.customersPerHour.push(randomCustomers(this.minCustomers, this.maxCustomers)); //populatecustomers is pushing the array of 14 numbers up into customers per hour []
+//     }
+//    };
+   
+// CookieStore.prototype.calculateCookiesPerHour = function () {
+//     this.populateCustomers();
+//     for (var i = 0; i < hours.length; i++) {
+//       this.cookiesPerHour.push(Math.ceil(this.customersPerHour[i] * this.averageCookies));
+//       this.totalCookies = this.totalCookies + this.cookiesPerHour[i];
+//     }
+//    };
